c2852997229300ba26897c13ffb868df1190f9cd
thebjorn <bp@datakortet.no>
2018-12-31 21:55:22 +0100

    wip

---
+++ src/help.js
@@ -0,0 +1,9 @@
+// LVL:0  [dk.env.DEBUG]
+
+const helptext = {
+    Class: "the dk root class",
+    counter: `Return a unique counter value.
+        The optional 'name' parameter, will create a label
+        'name' <counter>, where the counter starts from startval
+        (default 1).`
+};
