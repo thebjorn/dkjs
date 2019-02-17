4bafec540e71151c64cab73da27de9da13f631a7
thebjorn <bp@datakortet.no>
2019-02-17 03:04:35 +0100

    Test options array path.

--- src/forms/__tests__/test-radioselect.js
+++ src/forms/__tests__/test-radioselect.js
@@ -4,7 +4,7 @@ import page from "../../widgetcore/dk-page";
 import utidy from "../../browser/dk-html";
 
 
-test("select-widget", () => {
+test("radio-select-widget", () => {
     document.body.innerHTML = `
         <div id="work">
             
@@ -14,10 +14,10 @@ test("select-widget", () => {
     page.initialize(document);
 
     const w = RadioSelectWidget.create_on(work, {
-        options: {
-            1: 'hello',
-            2: 'world'
-        }
+        options: [
+            'hello',
+            'world'
+        ]
     });
     console.log(w);
     console.log(w.toString());
@@ -27,15 +27,15 @@ test("select-widget", () => {
     console.log(work.html());
 
     expect(w.formatted_value()).toBe('hello');
-    expect(w.get_field_value()).toMatchObject({v: "1", f: 'hello'});
+    expect(w.get_field_value()).toMatchObject({v: "0", f: 'hello'});
 
     expect(utidy(work.html())).toEqual(utidy(`
         <label for="work" id="work-label">
-            <input id="work" name="work" type="radio" value="1">
+            <input id="work" name="work" type="radio" value="0">
             &nbsp;hello
         </label>
         <label for="work" id="work-label">
-            <input id="work" name="work" type="radio" value="2">
+            <input id="work" name="work" type="radio" value="1">
             &nbsp;world
         </label>
     `));
