142b9351af3d27e1df4418ad51e2b3f7ea3e5eb5
Erlend Dalen <ed@norsktest.no>
2019-10-28 15:26:12 +0100

    Make sure we can easily set the pagesize also for the ajaxdatasource.

--- src/widgets/table/data-table.js
+++ src/widgets/table/data-table.js
@@ -101,15 +101,19 @@ export class DataTable extends Widget {
             this.table_data = new DataSet(dsprops);
         }
         if (this.table_data === null) {
-            this.data_url = (this.dk && this.dk.url)? this.dk.url : "";
+            let pagesize=25;  //default value
+            if (this.pagesize)
+                pagesize=this.pagesize;             
+                  
             //dk.info("URL", this.data_url);
             // if data is not defined, then create one
             this.table_data = new DataSet({
                 // default data source is AjaxDataSource
                 datasource: new AjaxDataSource({
                     // default url is current url
-                    url: self.data_url
-                })
+                    url: self.data_url,
+                }),
+                pagesize: pagesize
             });
         }
         if (!this.data_url) this.data_url = this.table_data.datasource.url;
