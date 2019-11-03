256b3595eefab7ddaa84d94a3538f139a01e5669
thebjorn <bp@datakortet.no>
2019-11-03 20:21:02 +0100

    Make PostnrLookupWidget work with remote ajax. Skip the url template yuchiness. Make the widget know about dkaddress fields (this widget should probably be in dkaddress instead of here...)

--- src/widgets/postnr-lookup.js
+++ src/widgets/postnr-lookup.js
@@ -7,41 +7,49 @@
  *      });
  */
 import {Widget} from "../widgetcore/dk-widget";
+import dk from "../dk-obj";
 
+/**
+ * Look up a Norwegian zip code and return the city.
+ * Create it on an item that only contains one address form (then you don't need to specify any parameters).
+ * If more than one form exists you will need to provide the postcode_id and city_id.
+ * 
+ * If you use the {% edit_address .. %} and {% add_address .. %} template tags from dkaddress, all of this
+ * is done for you automatically.
+ */
 export class PostnrLookupWidget extends Widget {
     constructor(...args) {
         super({
-            // type: 'postnr-lookup-widget',
-
-            // XXX: move into constructor?
             zipcode: null,  // url paramter
             postcode_id: null,
             city_id: null,
-
-            url: '//cache.norsktest.no/ajax/poststed/<%= zipcode %>/',
-            urldata: {
-                zipcode: function () { return this.zipcode || undefined; }
-            },
+            postcode_url: () => `//cache.norsktest.no/ajax/poststed/${this.zipcode}/`,
         }, ...args);
     }
 
     construct() {
-        this.postnr = this.widget(this.postcode_id);
-        this.poststed = this.widget(this.city_id);
-        this.poststed.attr('tabindex', 7);
-        this.zipcode = this.postnr.val();
+        this.$postnr = this.postcode_id ? this.widget(this.postcode_id) : this.widget('[name=postcode]');
+        this.$poststed = this.city_id ? this.widget(this.city_id) : this.widget('[name=city]');
+        this.$poststed.attr('tabindex', 7);
+        this.zipcode = this.$postnr.val();
     }
 
     draw(poststed) {
-        this.poststed.val(poststed || '');
+        if (poststed !== undefined) this.$poststed.val(poststed || '');
     }
 
     handlers() {
         const self = this;
-
-        self.postnr.blur(function () {
-            self.zipcode = self.postnr.val();
-            self.refresh();
+        
+        this.$postnr.on('blur', () => {
+            this.zipcode = this.$postnr.val();
+            dk.$.ajax({  // can't use dk.ajax here, since we're doing a cors call
+                cache: false,
+                url: self.postcode_url(),
+                success(data) {
+                    self.draw(data);
+                }
+            });
         });
     }
 }
