84df5a80883f99a6d470ca0d42d09140ff840ac7
thebjorn <bp@datakortet.no>
2019-11-03 21:11:46 +0100

    upversion

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = u'2018, Bjorn Pettersen'
 author = u'Bjorn Pettersen'
 
 # The short X.Y version
-version = u'3.0.3'
+version = u'3.0.4'
 # The full version, including alpha/beta/rc tags
-release = u'3.0.3'
+release = u'3.0.4'
 
 
 # -- General configuration ---------------------------------------------------
