16bcd4b2e368b2f522ee84bf2af440732df81d0a
thebjorn <bp@datakortet.no>
2019-11-03 21:09:39 +0100

    new prod version

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = u'2018, Bjorn Pettersen'
 author = u'Bjorn Pettersen'
 
 # The short X.Y version
-version = u'3.0.0'
+version = u'3.0.3'
 # The full version, including alpha/beta/rc tags
-release = u'3.0.0'
+release = u'3.0.3'
 
 
 # -- General configuration ---------------------------------------------------
