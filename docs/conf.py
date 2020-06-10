99f9028298d82aa7132b8df1a5a89bf20cd8f122
Erlend Dalen <ed@norsktest.no>
2020-06-10 08:20:05 +0200

    upversion

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = u'2018, Bjorn Pettersen'
 author = u'Bjorn Pettersen'
 
 # The short X.Y version
-version = u'3.0.17'
+version = u'3.0.18'
 # The full version, including alpha/beta/rc tags
-release = u'3.0.17'
+release = u'3.0.18'
 
 
 # -- General configuration ---------------------------------------------------
