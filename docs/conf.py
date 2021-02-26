8f82a06edf823023938746185f0f998c210e0fec
Bjorn Pettersen <bp@norsktest.no>
2021-02-26 11:21:14 +0100

    upversion

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = u'2018, Bjorn Pettersen'
 author = u'Bjorn Pettersen'
 
 # The short X.Y version
-version = u'3.0.25'
+version = u'3.0.26'
 # The full version, including alpha/beta/rc tags
-release = u'3.0.25'
+release = u'3.0.26'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ Topic :: Software Development :: Libraries
 
 setuptools.setup(
     name='dkdj',
-    version='3.0.25',
+    version='3.0.26',
     packages=setuptools.find_packages(exclude=['tests']),
     install_requires=[
         'ttcal',
